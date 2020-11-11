104f1aff58bbc9e215308526fa368de2b8c64c4f
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-11-11 18:14:50 +0900

    Added migration for news table

---
+++ console/migrations/m201111_090612_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news}}`.
+ */
+class m201111_090612_create_news_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%news}}', [
+            'id' => $this->primaryKey(),
+            'category_id' => $this->integer()->notNull(),
+            'name' => $this->string()->notNull(),
+            'title' => $this->string()->notNull(),
+            'content' => $this->text(),
+            'slug' => $this->string()->notNull(),
+            'created_at' => $this->integer()->unsigned()->notNull(),
+            'meta_json' => $this->json()->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex('{{%idx-news-slug}}', '{{%news}}', 'slug', true);
+        $this->createIndex('{{%idx-news-category_id}}', '{{%news}}', 'category_id');
+        $this->addForeignKey('{{%fk-news-category_id}}', '{{%news}}', 'category_id', '{{%category}}', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%news}}');
+    }
+}
