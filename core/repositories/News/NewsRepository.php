57f20e2fa46482e0ce246029da36a22237b55b5a
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-11-11 18:46:25 +0900

    Added NewsRepository

---
+++ core/repositories/News/NewsRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace core\repositories\News;
+
+use core\entities\News\News;
+use core\repositories\NotFoundException;
+
+class NewsRepository
+{
+    public function get($id): News
+    {
+        if (!$news = News::findOne($id)) {
+            throw new NotFoundException('News is not found.');
+        }
+        return $news;
+    }
+
+    /**
+     * @param News $news
+     */
+    public function save(News $news): void
+    {
+        if (!$news->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+
+    /**
+     * @param News $news
+     */
+    public function remove(News $news): void
+    {
+        if (!$news->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
+}
